b908fa2e44395ccef77ea656b9747f7d9460ac52
dansodies2608 <danso12345@gmail.com>
2025-04-30 12:16:14 +0300

    Create firebase-messaging-sw.js

---
+++ assets/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+// Инициализация Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAW2SdDTCpt25PVoB7ROt-tiVrFuabwE4I",
+    authDomain: "petrovkiy-v1.firebaseapp.com",
+    projectId: "petrovkiy-v1",
+    storageBucket: "petrovkiy-v1.firebasestorage.app",
+    messagingSenderId: "146966889113",
+    appId: "1:146966889113:web:e0c92825038949959dae08"
+});
+
+const messaging = firebase.messaging();
+
+// Обработка фоновых уведомлений (когда приложение закрыто)
+messaging.onBackgroundMessage((payload) => {
+  console.log('Уведомление получено в фоне:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png',
+    vibrate: [200, 100, 200] // Вибрация на мобильных
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
